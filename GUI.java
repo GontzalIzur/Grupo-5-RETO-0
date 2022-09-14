5e7aa6304b9832d9ea74a1118e008e3c9224cf43
GontzalIzur <gontzal.izurzare@elorrieta-errekamari.com>
2022-09-14 08:50:49 +0200

    bruh

--- GUI.java
+++ GUI.java
@@ -1,6 +1,13 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
 
 public class GUI {
 
@@ -34,8 +41,18 @@ public class GUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1017, 768);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frame.getContentPane().add(tabbedPane, BorderLayout.NORTH);
+		
+		JPanel Calefacciones = new JPanel();
+		tabbedPane.addTab("Calefacciones", null, Calefacciones, null);
+		
+		JPanel Alarmas = new JPanel();
+		Alarmas.setBounds(100, 100, 450, 300);
+		tabbedPane.addTab("Alarmas", null, Alarmas, null);
+		Alarmas.setLayout(null);
 	}
-
 }
