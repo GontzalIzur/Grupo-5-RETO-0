5a2eb37e4a12ae9faccc48caebb7617d1a620a49
GontzalIzur <gontzal.izurzare@elorrieta-errekamari.com>
2022-09-15 12:55:09 +0200

    Icon

--- GUI.java
+++ GUI.java
@@ -45,6 +45,7 @@ public class GUI extends JFrame implements ActionListener {
 		contentPane = new JPanel();
 		setTitle("Mapa de salas");
 		setResizable(false);
+		setIconImage(new ImageIcon(getClass().getResource("Media/Plano.jpg")).getImage());
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
