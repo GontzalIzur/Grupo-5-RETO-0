89aea3b67508f53c31c78e904af833f61d3fc013
boguminillo <boguminillo@gmail.com>
2022-09-15 12:59:03 +0100

    actionListener agregado a las alarmas

--- GUI.java
+++ GUI.java
@@ -276,174 +276,203 @@ public class GUI extends JFrame implements ActionListener {
 		JButton TallerMantenimiento_1 = new JButton("");
 		TallerMantenimiento_1.setName("TallerMantenimiento");
 		TallerMantenimiento_1.setBackground(mapa.get("TallerMantenimiento")[1] ? Color.GREEN : Color.RED);
+		TallerMantenimiento_1.addActionListener(this);
 		TallerMantenimiento_1.setBounds(354, 135, 18, 18);
 		alarmas.add(TallerMantenimiento_1);
 
 		JButton Aula014_1 = new JButton("");
 		Aula014_1.setName("Aula014");
 		Aula014_1.setBackground(mapa.get("Aula014")[1] ? Color.GREEN : Color.RED);
+		Aula014_1.addActionListener(this);
 		Aula014_1.setBounds(260, 316, 18, 18);
 		alarmas.add(Aula014_1);
 
 		JButton Aula012_1 = new JButton("");
 		Aula012_1.setName("Aula012");
 		Aula012_1.setBackground(mapa.get("Aula012")[1] ? Color.GREEN : Color.RED);
+		Aula012_1.addActionListener(this);
 		Aula012_1.setBounds(423, 304, 18, 18);
 		alarmas.add(Aula012_1);
 
 		JButton Aula013_1 = new JButton("");
 		Aula013_1.setName("Aula013");
 		Aula013_1.setBackground(mapa.get("Aula013")[1] ? Color.GREEN : Color.RED);
+		Aula013_1.addActionListener(this);
 		Aula013_1.setBounds(342, 304, 18, 18);
 		alarmas.add(Aula013_1);
 
 		JButton PasoAula013_1 = new JButton("");
 		PasoAula013_1.setName("PasoAula013");
 		PasoAula013_1.setBackground(mapa.get("PasoAula013")[1] ? Color.GREEN : Color.RED);
+		PasoAula013_1.addActionListener(this);
 		PasoAula013_1.setBounds(402, 347, 18, 18);
 		alarmas.add(PasoAula013_1);
 
 		JButton Hall_1 = new JButton("");
 		Hall_1.setName("Hall");
 		Hall_1.setBackground(mapa.get("Hall")[1] ? Color.GREEN : Color.RED);
+		Hall_1.addActionListener(this);
 		Hall_1.setBounds(454, 374, 18, 18);
 		alarmas.add(Hall_1);
 
 		JButton Almacen1_1 = new JButton("");
 		Almacen1_1.setName("Almacen1");
 		Almacen1_1.setBackground(mapa.get("Almacen1")[1] ? Color.GREEN : Color.RED);
+		Almacen1_1.addActionListener(this);
 		Almacen1_1.setBounds(532, 374, 18, 18);
 		alarmas.add(Almacen1_1);
 
 		JButton Almacen2_1 = new JButton("");
 		Almacen2_1.setName("Almacen2");
 		Almacen2_1.setBackground(mapa.get("Almacen2")[1] ? Color.GREEN : Color.RED);
+		Almacen2_1.addActionListener(this);
 		Almacen2_1.setBounds(585, 374, 18, 18);
 		alarmas.add(Almacen2_1);
 
 		JButton Almacen3_1 = new JButton("");
 		Almacen3_1.setName("Almacen3");
 		Almacen3_1.setBackground(mapa.get("Almacen3")[1] ? Color.GREEN : Color.RED);
+		Almacen3_1.addActionListener(this);
 		Almacen3_1.setBounds(847, 374, 18, 18);
 		alarmas.add(Almacen3_1);
 
 		JButton Aula015_1 = new JButton("");
 		Aula015_1.setName("Aula015");
 		Aula015_1.setBackground(mapa.get("Aula015")[1] ? Color.GREEN : Color.RED);
+		Aula015_1.addActionListener(this);
 		Aula015_1.setBounds(433, 485, 18, 18);
 		alarmas.add(Aula015_1);
 
 		JButton Aula010_1 = new JButton("");
 		Aula010_1.setName("Aula010");
 		Aula010_1.setBackground(mapa.get("Aula010")[1] ? Color.GREEN : Color.RED);
+		Aula010_1.addActionListener(this);
 		Aula010_1.setBounds(532, 472, 18, 18);
 		alarmas.add(Aula010_1);
 
 		JButton Aula016_1 = new JButton("");
 		Aula016_1.setName("Aula016");
 		Aula016_1.setBackground(mapa.get("Aula016")[1] ? Color.GREEN : Color.RED);
+		Aula016_1.addActionListener(this);
 		Aula016_1.setBounds(433, 562, 18, 18);
 		alarmas.add(Aula016_1);
 
 		JButton Archivo_1 = new JButton("");
 		Archivo_1.setName("Archivo");
 		Archivo_1.setBackground(mapa.get("Archivo")[1] ? Color.GREEN : Color.RED);
+		Archivo_1.addActionListener(this);
 		Archivo_1.setBounds(520, 543, 18, 18);
 		alarmas.add(Archivo_1);
 
 		JButton PasoAula016_1 = new JButton("");
 		PasoAula016_1.setName("PasoAula016");
 		PasoAula016_1.setBackground(mapa.get("PasoAula016")[1] ? Color.GREEN : Color.RED);
+		PasoAula016_1.addActionListener(this);
 		PasoAula016_1.setBounds(489, 500, 18, 18);
 		alarmas.add(PasoAula016_1);
 
 		JButton AseoMinusval_1 = new JButton("");
 		AseoMinusval_1.setName("AseoMinusval");
 		AseoMinusval_1.setBackground(mapa.get("AseoMinusval")[1] ? Color.GREEN : Color.RED);
+		AseoMinusval_1.addActionListener(this);
 		AseoMinusval_1.setBounds(532, 511, 18, 18);
 		alarmas.add(AseoMinusval_1);
 
 		JButton Aula007_1 = new JButton("");
 		Aula007_1.setName("Aula007");
 		Aula007_1.setBackground(mapa.get("Aula007")[1] ? Color.GREEN : Color.RED);
+		Aula007_1.addActionListener(this);
 		Aula007_1.setBounds(585, 579, 18, 18);
 		alarmas.add(Aula007_1);
 
 		JButton Aula006_1 = new JButton("");
 		Aula006_1.setName("Aula006");
 		Aula006_1.setBackground(mapa.get("Aula006")[1] ? Color.GREEN : Color.RED);
+		Aula006_1.addActionListener(this);
 		Aula006_1.setBounds(683, 579, 18, 18);
 		alarmas.add(Aula006_1);
 
 		JButton Vestibulo_1 = new JButton("");
 		Vestibulo_1.setName("Vestibulo");
 		Vestibulo_1.setBackground(mapa.get("Vestibulo")[1] ? Color.GREEN : Color.RED);
+		Vestibulo_1.addActionListener(this);
 		Vestibulo_1.setBounds(671, 648, 18, 18);
 		alarmas.add(Vestibulo_1);
 
 		JButton PasoVestibulo_1 = new JButton("");
 		PasoVestibulo_1.setName("PasoVestibulo");
 		PasoVestibulo_1.setBackground(mapa.get("PasoVestibulo")[1] ? Color.GREEN : Color.RED);
+		PasoVestibulo_1.addActionListener(this);
 		PasoVestibulo_1.setBounds(561, 613, 18, 18);
 		alarmas.add(PasoVestibulo_1);
 
 		JButton SalaAlumnos_1 = new JButton("");
 		SalaAlumnos_1.setName("SalaAlumnos");
 		SalaAlumnos_1.setBackground(mapa.get("SalaAlumnos")[1] ? Color.GREEN : Color.RED);
+		SalaAlumnos_1.addActionListener(this);
 		SalaAlumnos_1.setBounds(768, 660, 18, 18);
 		alarmas.add(SalaAlumnos_1);
 
 		JButton SalaProfesores_1 = new JButton("");
 		SalaProfesores_1.setName("SalaProfesores");
 		SalaProfesores_1.setBackground(mapa.get("SalaProfesores")[1] ? Color.GREEN : Color.RED);
+		SalaProfesores_1.addActionListener(this);
 		SalaProfesores_1.setBounds(847, 660, 18, 18);
 		alarmas.add(SalaProfesores_1);
 
 		JButton Aula003_1 = new JButton("");
 		Aula003_1.setName("Aula003");
 		Aula003_1.setBackground(mapa.get("Aula003")[1] ? Color.GREEN : Color.RED);
+		Aula003_1.addActionListener(this);
 		Aula003_1.setBounds(898, 660, 18, 18);
 		alarmas.add(Aula003_1);
 
 		JButton DPTInformatica_1 = new JButton("");
 		DPTInformatica_1.setName("DPTInformatica");
 		DPTInformatica_1.setBackground(mapa.get("DPTInformatica")[1] ? Color.GREEN : Color.RED);
+		DPTInformatica_1.addActionListener(this);
 		DPTInformatica_1.setBounds(898, 579, 18, 18);
 		alarmas.add(DPTInformatica_1);
 
 		JButton DPTFOL_1 = new JButton("");
 		DPTFOL_1.setName("DPTFOL");
 		DPTFOL_1.setBackground(mapa.get("DPTFOL")[1] ? Color.GREEN : Color.RED);
+		DPTFOL_1.addActionListener(this);
 		DPTFOL_1.setBounds(811, 579, 18, 18);
 		alarmas.add(DPTFOL_1);
 
 		JButton Aula005B_1 = new JButton("");
 		Aula005B_1.setName("Aula005B");
 		Aula005B_1.setBackground(mapa.get("Aula005B")[1] ? Color.GREEN : Color.RED);
+		Aula005B_1.addActionListener(this);
 		Aula005B_1.setBounds(768, 579, 18, 18);
 		alarmas.add(Aula005B_1);
 
 		JButton VestuarioLimpieza_1 = new JButton("");
 		VestuarioLimpieza_1.setName("VestuarioLimpieza");
 		VestuarioLimpieza_1.setBackground(mapa.get("VestuarioLimpieza")[1] ? Color.GREEN : Color.RED);
+		VestuarioLimpieza_1.addActionListener(this);
 		VestuarioLimpieza_1.setBounds(157, 579, 18, 18);
 		alarmas.add(VestuarioLimpieza_1);
 
 		JButton AseoFem_1 = new JButton("");
 		AseoFem_1.setName("AseoFem");
 		AseoFem_1.setBackground(mapa.get("AseoFem")[1] ? Color.GREEN : Color.RED);
+		AseoFem_1.addActionListener(this);
 		AseoFem_1.setBounds(219, 596, 18, 18);
 		alarmas.add(AseoFem_1);
 
 		JButton AseoMasc_1 = new JButton("");
 		AseoMasc_1.setName("AseoMasc");
 		AseoMasc_1.setBackground(mapa.get("AseoMasc")[1] ? Color.GREEN : Color.RED);
+		AseoMasc_1.addActionListener(this);
 		AseoMasc_1.setBounds(273, 596, 18, 18);
 		alarmas.add(AseoMasc_1);
 
 		JButton Conserjeria_1 = new JButton("");
 		Conserjeria_1.setName("Conserjeria");
 		Conserjeria_1.setBackground(mapa.get("Conserjeria")[1] ? Color.GREEN : Color.RED);
+		Conserjeria_1.addActionListener(this);
 		Conserjeria_1.setBounds(608, 667, 18, 18);
 		alarmas.add(Conserjeria_1);
 
