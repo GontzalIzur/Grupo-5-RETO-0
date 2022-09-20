16b100b348b6abaa92d3fac3492b901f1c6c9fc2
GontzalIzur <gontzal.izurzare@elorrieta-errekamari.com>
2022-09-20 13:11:57 +0200

    #

--- GUI.java
+++ GUI.java
@@ -8,7 +8,6 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.Color;
